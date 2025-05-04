6ec4a14d7c1d5c2534794675fc63e5fff1e5afc2
0fuguiwang0 <1477021760@qq.com>
2025-05-04 14:33:50 +0800

    Finish Learn Seata, 为了能让seata和service在同一个项目, 把service底下的spring-cloud-starter-alibaba-seata,抽出来单独都放在各自seata子模块pom.xml上了, 不然会因为service模块缺少file.conf而报错

---
+++ services/seata-storage/src/main/java/com/rabbit/storage/SeataStorageMainApplication.java
@@ -0,0 +1,19 @@
+package com.rabbit.storage;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@EnableTransactionManagement
+@MapperScan("com.rabbit.storage.mapper")
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SeataStorageMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataStorageMainApplication.class, args);
+    }
+}
